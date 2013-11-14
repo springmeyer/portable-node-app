ba96a093d180957e2658fea12ec40470a4160b47
Dane Springmeyer <dane@dbsgeo.com>
2013-11-14 14:08:35 -0800

    scripting setup

--- build.bat
+++ build.bat
@@ -1,9 +1,16 @@
+@rem settings
 set DEST=tilemill-win-0.9.1
 set SOURCE=tilemill
-mkdir %DEST%
+
+@rem cleanup
 rd /q /s %DEST%
+del %DEST%.zip
+
+@rem build
+mkdir .\%DEST%\
 xcopy /i /s /exclude:excludes.txt %SOURCE% .\%DEST%\%SOURCE%
 copy TileMill.exe .\%DEST%\
 copy install_me_first.exe .\%DEST%\
 copy README.txt .\%DEST%\
+7z a %DEST%.zip .\%DEST%\
 pause
